6e56b07b4e7be30b2b8ae364ec984e017d1035dc
dennomwas <githinji.mwangi@gmail.com>
2018-10-09 23:30:13 +0300

    [Ft] Add Error handling middleware
    - Create /questions/answers routes
    - Create /questions/answers/vote routes

--- app.js
+++ app.js
@@ -2,19 +2,40 @@
 
 const express = require('express');
 const jsonParser = require('body-parser').json;
+const logger = require('morgan');
 const app = express();
 
 // local imports
-const route = require('./routes')
+const route = require('./routes') // import all routes
+
+// to log http status codes to console
+app.use(logger('dev'));
+
+// parse incoming requests to json 
+app.use(jsonParser());
+
+// create our routes
+app.use('/questions', route);
+
+// catch 404 errors
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// Error handlers
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 // define server port
 const port = process.env.PORT || 3000;
-
-// parse incoming json requests
-app.use(jsonParser());
-
-// create main routes
-app.use('/questions', route);
 
 // run the express server
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -20,11 +20,70 @@ router.post('/', (req, res) => {
     })
 });
 
-// GET /questions/:id 
+// GET /questions/:qID 
 // Route to a specific question
-router.get('/:id', (req, res) => {
+router.get('/:qID', (req, res) => {
     res.json({
-        response: "You sent a GET request to ID:" + req.params.id
+        response: "You sent a GET request to qID:" + req.params.qID
+    })
+});
+
+// POST /questions/:qID/answers 
+// Route to post an answer to a question
+router.post('/:qID/answers', (req, res) => {
+    res.json({
+        response: "You sent a POST request",
+        body: req.body,
+        questionID: req.params.qID
+    })
+});
+
+// GET /questions/:qID/answers/:aID
+// Route to a specific answer to a question
+router.get('/:qID/answers/:aID', (req, res) => {
+    res.json({
+        response: `You sent a GET request to /${req.params.qID}/answers/${req.params.aID}`,
+        questionID: req.params.qID,
+        answerID: req.params.aID
+    })
+});
+
+// PUT /questions/:qID/answers/:aID
+// Route to update a specific answer to a question
+router.put('/:qID/answers/:aID', (req, res) => {
+    res.json({
+        response: `You sent a PUT request to /${req.params.qID}/answers/${req.params.aID}`,
+        questionID: req.params.qID,
+        body: req.body,
+        answerID: req.params.aID
+    })
+});
+
+// DELETE /questions/:qID/answers/:aID
+// Route to delete a specific answer to a question
+router.delete('/:qID/answers/:aID', (req, res) => {
+    res.json({
+        response: `You sent a DELETE request to /${req.params.qID}/answers/${req.params.aID}`,
+        questionID: req.params.qID,
+        answerID: req.params.aID
+    })
+});
+
+// GET /questions/:qID/answers/:aID/vote-up
+// Route to vote a specific answer to a question
+router.post('/:qID/answers/:aID/vote-:dir', (req, res, next) => {
+    if (req.params.dir.search(/^(up|down)$/) === -1) {
+        const err = new Error('Not Found!');
+        err.status = 404;
+        next(err);
+    }
+    next()
+}, (req, res) => {
+    res.json({
+        response: `You sent a Vote request to /${req.params.qID}/answers/${req.params.aID}/vote-${req.params.dir}`,
+        questionID: req.params.qID,
+        answerID: req.params.aID,
+        vote: req.params.dir
     })
 });
 
